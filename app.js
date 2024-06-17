753e458e5c41db58b2e01a9bca0ec7501cfb7912
Raza2715 <146386171+Raza2715@users.noreply.github.com>
2024-06-17 18:31:41 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressSession = require('express-session');
+const flash = require('connect-flash');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const passport = require('passport');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(expressSession({
+ resave :false,
+ saveUninitialized : false,
+ secret :"batao batao"
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(usersRouter.serializeUser());
+passport.deserializeUser(usersRouter.deserializeUser());
+app.use(flash());
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
+
+
+
